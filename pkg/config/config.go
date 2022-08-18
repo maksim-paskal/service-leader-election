90153bbbe2cd145c6221ad31121ea60a4cff5fa8
Maksim Paskal <paskal.maksim@gmail.com>
2022-08-18 07:57:07 +0300

    watch container health
    Signed-off-by: Maksim Paskal <paskal.maksim@gmail.com>

---
+++ pkg/config/config.go
@@ -0,0 +1,34 @@
+/*
+Copyright paskal.maksim@gmail.com
+Licensed under the Apache License, Version 2.0 (the "License")
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package config
+
+import (
+	"flag"
+	"os"
+)
+
+const defaultPort = 28086
+
+var (
+	LeaseName             = flag.String("lease-name", "service-leader-election", "name of lease to be created")
+	CanPatchService       = flag.Bool("patch-service", true, "patch service with pod labels")
+	ServiceName           = flag.String("service-name", "service-leader-election", "name of service to be patch")
+	ServiceKey            = flag.String("service-key", "service-leader-election", "key of service to be patch")
+	Podname               = flag.String("podname", os.Getenv("POD_NAME"), "name of the pod")
+	Namespace             = flag.String("namespace", os.Getenv("POD_NAMESPACE"), "namespace of the pod")
+	ContainerName         = flag.String("container-name", "", "name of the container to watch")
+	Kubeconfig            = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	Port                  = flag.Int("web.port", defaultPort, "port to listen on")
+	CheckForReady         = flag.Bool("check-for-ready", true, "check for pod primary container readiness")
+	CheckForReadyInterval = flag.Duration("check-for-ready-interval", 1, "interval to check for pod primary container readiness") //nolint:lll
+)
--- pkg/web/web.go
+++ pkg/web/web.go
@@ -1,25 +1,34 @@
+/*
+Copyright paskal.maksim@gmail.com
+Licensed under the Apache License, Version 2.0 (the "License")
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package web
 
 import (
-	"flag"
 	"fmt"
 	"net/http"
 	"os"
 
+	"github.com/maksim-paskal/service-leader-election/pkg/api"
+	"github.com/maksim-paskal/service-leader-election/pkg/config"
 	log "github.com/sirupsen/logrus"
 	"go.uber.org/atomic"
 )
 
-const defaultPort = 28086
-
-var port = flag.Int("web.port", defaultPort, "port to listen on")
-
 var IsMaster atomic.Bool
 
 func StartServer() {
-	log.Info(fmt.Sprintf("Starting on port %d...", *port))
+	log.Info(fmt.Sprintf("Starting on port %d...", *config.Port))
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", *port), GetHandler())
+	err := http.ListenAndServe(fmt.Sprintf(":%d", *config.Port), GetHandler())
 	if err != nil {
 		log.WithError(err).Fatal()
 	}
@@ -46,6 +55,21 @@ func handlerReady(w http.ResponseWriter, r *http.Request) {
 }
 
 func handlerHealthz(w http.ResponseWriter, r *http.Request) {
+	ready, container, err := api.CheckContainerIsReady(r.Context())
+	if err != nil {
+		log.WithError(err).Errorf("failed to check container %s ready status", container)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+
+		return
+	}
+
+	// liveness probe will fail if primary container is not ready.
+	if !ready {
+		http.Error(w, "primary container is not ready", http.StatusInternalServerError)
+
+		return
+	}
+
 	_, _ = w.Write([]byte("live"))
 }
 
